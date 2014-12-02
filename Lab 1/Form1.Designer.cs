9a32b31de5ccf20d6df0b71196825027ced24fae
Whitney Haddow <whitneyhaddow@gmail.com>
2014-12-02 13:10:37 -0700

    Order form for various t-shirts
    Loads product information from text file and allows user to enter in an
    order.

---
+++ Lab 1/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstShirts = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.cboStyle = new System.Windows.Forms.ComboBox();
+            this.cboSize = new System.Windows.Forms.ComboBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "T-shirts (Styles, Sizes, Prices)";
+            // 
+            // lstShirts
+            // 
+            this.lstShirts.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstShirts.FormattingEnabled = true;
+            this.lstShirts.ItemHeight = 16;
+            this.lstShirts.Location = new System.Drawing.Point(21, 52);
+            this.lstShirts.Name = "lstShirts";
+            this.lstShirts.Size = new System.Drawing.Size(624, 132);
+            this.lstShirts.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Order Selection";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Style";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(227, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Size";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(539, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Order Quantity";
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSubtotal.Location = new System.Drawing.Point(492, 339);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(153, 23);
+            this.lblSubtotal.TabIndex = 6;
+            this.lblSubtotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTax
+            // 
+            this.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTax.Location = new System.Drawing.Point(492, 367);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(153, 22);
+            this.lblTax.TabIndex = 7;
+            this.lblTax.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotal.Location = new System.Drawing.Point(492, 395);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(153, 22);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // cboStyle
+            // 
+            this.cboStyle.FormattingEnabled = true;
+            this.cboStyle.Location = new System.Drawing.Point(21, 254);
+            this.cboStyle.Name = "cboStyle";
+            this.cboStyle.Size = new System.Drawing.Size(203, 24);
+            this.cboStyle.TabIndex = 9;
+            // 
+            // cboSize
+            // 
+            this.cboSize.FormattingEnabled = true;
+            this.cboSize.Location = new System.Drawing.Point(230, 254);
+            this.cboSize.Name = "cboSize";
+            this.cboSize.Size = new System.Drawing.Size(92, 24);
+            this.cboSize.TabIndex = 10;
+            this.cboSize.SelectedIndexChanged += new System.EventHandler(this.cboSize_SelectedIndexChanged);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(494, 254);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(151, 23);
+            this.txtQuantity.TabIndex = 11;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(494, 289);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(151, 38);
+            this.btnCalculate.TabIndex = 12;
+            this.btnCalculate.Text = "&Calculate Total";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.UseWaitCursor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(422, 346);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Subtotal";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(454, 373);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Tax";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(444, 401);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Total";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(542, 434);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(103, 38);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.UseWaitCursor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(425, 434);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(103, 38);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.UseWaitCursor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Enabled = false;
+            this.txtUnitPrice.Location = new System.Drawing.Point(328, 254);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(113, 23);
+            this.txtUnitPrice.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(325, 228);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 16);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Unit Price";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(684, 495);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.cboSize);
+            this.Controls.Add(this.cboStyle);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstShirts);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "ABC Textile Orders";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstShirts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.ComboBox cboStyle;
+        private System.Windows.Forms.ComboBox cboSize;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Lab 1/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab_1
+/***************************************************
+* This form calculates total price for an order
+* based on t-shirt style, size, and quantity.
+* Created November 2014 by Whitney Haddow
+ * Used the following webpage for help splitting data file into 2D array:
+ * http://stackoverflow.com/questions/9674469/create-2d-array-from-txt-file
+****************************************************/
+{
+    public partial class Form1 : Form
+    {
+        //declare the arrays
+        string[] sizes = { "S", "M", "L", "XL" };
+        int countSizes; // how many sizes
+        string[] styles = { "Plain Black/White", "Plain Colour", "White w/Black Logo", "Colour w/Black Logo", "Black w/Sliver Logo", "Colour w/Silver Logo" };
+        int countStyles; // how many styles
+        decimal[,] prices; // two dimensional array of prices
+
+        //declare constants
+        const decimal TAX_PERCENT = 0.05m;
+
+        //global variables
+        decimal unitPrice;
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // find number of styles and sizes
+            countSizes = sizes.Length;
+            countStyles = styles.Length;
+                   
+            // create two dimensional array
+            prices = new decimal[countStyles, countSizes];
+
+            //read the prices file, store in array, display in listbox
+            LoadData();
+
+            //load the style types into cboStyle - plain black/white selected
+            for (int i = 0; i < countStyles; i++)
+            {
+                cboStyle.Items.Add(styles[i]);
+            }
+            cboStyle.SelectedIndex = 0;
+
+            //load the sizes into cboSize - small selected
+            for (int j = 0; j < countSizes; j++)
+            {
+                cboSize.Items.Add(sizes[j]);
+            }
+            cboSize.SelectedIndex = 0;
+
+            //populate unit price
+            unitPrice = ReturnUnitPrice();
+            txtUnitPrice.Text = String.Format("{0:c}", unitPrice);
+            
+        }
+
+        //change the unit price when style and size are selected
+        private void cboSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            unitPrice = ReturnUnitPrice();
+            txtUnitPrice.Text = String.Format("{0:c}", unitPrice);
+        }
+        
+        private bool LoadData()
+        {
+            string path = "ABC_TEXTILES_PRICES.txt";
+            FileStream fs;
+            StreamReader sr; // for file reading
+            try
+            {
+                // open the file
+                fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(fs);
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("The file was not found.");
+                return false;
+            }
+
+            // file opened successfully
+            // read file and store data into the array
+            try
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line; // one line from the file
+                    string[] parts; // line will be split into parts
+                    
+                    
+                    int x, s; // indexes for sizes and styles respectively
+                    lstShirts.Items.Clear(); // start with an empty list box
+                    string header = "Styles/Sizes";
+                    for (x = 0; x < countSizes; x++) //puts sizes across the top
+                        header += " \t\t " + sizes[x];
+                    lstShirts.Items.Add(header);
+                    lstShirts.Items.Add("----------------------------------------------------------------------------------------");
+                    string rows;
+                    for (s = 0; s < countStyles; s++) // for each row
+                    {
+                        line = sr.ReadLine(); // read next line
+                        parts = line.Split(','); // split the line into parts using comma as delimiter
+                        rows = styles[s] + "\t";
+                        if (styles[s].Length < 18) rows += "\t"; //for short style names add extra tab
+                        for (x = 0; x < countSizes; x++)
+                        {                          
+                            prices[s, x] = decimal.Parse(parts[x]);  
+                            rows += prices[s, x] + "\t\t"; // prices for given style and size
+                        }
+                        lstShirts.Items.Add(rows);
+                    }
+                }
+
+                return true;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("I/O error occurred while reading from the file: " +
+                            ex.Message);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unanticipated error occurred while reading from the  file: " +
+                            ex.Message);
+                return false;
+            }
+            finally
+            {
+                sr.Close(); // close the file
+            }
+        } // end LoadData
+
+       //method to determine the value of unitPrice
+        public decimal ReturnUnitPrice()
+        {
+            decimal unitPrice; //result
+            int cboStyleIndex = cboStyle.SelectedIndex;
+            int cboSizeIndex = cboSize.SelectedIndex;
+            unitPrice = prices[cboStyleIndex, cboSizeIndex];
+            return unitPrice;
+        }
+
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //read the inputs (cbo boxes and order quantity}
+            decimal quantity;
+            quantity = Convert.ToDecimal(txtQuantity.Text);
+            unitPrice = ReturnUnitPrice();
+                      
+            //calculate totals
+            if (IsNonNegativeDecimal(txtQuantity.Text))
+            {
+                decimal subtotal = unitPrice * quantity;
+                decimal tax = subtotal * TAX_PERCENT;
+                decimal total = subtotal + tax;
+
+
+                //display subtotal, tax, and total into the labels
+                lblSubtotal.Text = String.Format("{0:c}", subtotal);
+                lblTax.Text = String.Format("{0:c}", tax);
+                lblTotal.Text = String.Format("{0:c}", total);
+            }
+        }
+
+       
+        //validate order quantity
+        public bool IsNonNegativeDecimal(string input)
+        {
+            bool result = true; //default
+            if (input == "")
+            {
+                MessageBox.Show("Please enter order quantity.",
+                   "Error");
+                return false;
+            }
+            else //not empty
+            {
+                decimal val; //temporary value to tryparse
+                if (!decimal.TryParse(input, out val))
+                {
+                    MessageBox.Show("Order quantity must be entered as a number.",
+                   "Error");
+                    return false;
+                }
+                else
+                {
+                    if (val < 0)
+                    {
+                        MessageBox.Show("Order quantity must be entered as number above 0.",
+                       "Error");
+                        return false;
+                    }
+                    else
+                        return true;
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear all fields and reset cbo boxes
+            txtQuantity.Text = "";
+            lblSubtotal.Text = "";
+            lblTax.Text = "";
+            lblTotal.Text = "";
+            cboStyle.SelectedIndex = 0;
+            cboSize.SelectedIndex = 0;
+
+        }
+
+        //terminate application
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+             
+     }
+
+}
